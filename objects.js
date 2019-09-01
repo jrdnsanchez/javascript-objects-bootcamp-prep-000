55f2bd2979af4b0989fc719521615625c01dd1ec
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-09-01 23:37:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
 'Dean Martin': 'Everybody Loves Somebody',
 'Elton John': 'I am still standing'
 }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
