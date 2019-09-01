44605ded72442b852f9139e651b418b33a5ad107
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-09-01 23:36:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,11 +2,3 @@ var playlist = {
 'Dean Martin': 'Everybody Loves Somebody',
 'Elton John': 'I am still standing'
 }
-Function updatePlaylist (playlist, artistName, songTitle) {
-  playlist [artistName] = songTitle
-  return playlist
-}
-Function removeFromPlaylist (playlist,artistName) {
-  delete playlist[artistName]
-  return playlist
-}
